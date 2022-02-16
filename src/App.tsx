2189c99d77d3adae859d8b2deabf68b9de538ba4
Chiro Hiro <chiro8x@gmail.com>
2022-02-16 13:52:46 +0700

    Release new version of vesting

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import TableHead from '@mui/material/TableHead';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
 import { Container, Grid, Typography } from '@mui/material';
 import { BigNumber, ethers } from 'ethers';
 import TableVesting from './main-page/table-vesting';
@@ -153,11 +152,11 @@ export default class App extends React.Component {
           <WalletConnector onConnect={this.onConnect.bind(this)} />
           {wallet === null ? (
             <>
-              <Grid 
+              <Grid
                 container
-                mt="1rem" 
-                mb="1.5rem" 
-                sx={{padding: "1.5rem", backgroundColor: "#D0BC9C50", border: "2px solid #94763B"}}
+                mt="1rem"
+                mb="1.5rem"
+                sx={{ padding: '1.5rem', backgroundColor: '#D0BC9C50', border: '2px solid #94763B' }}
               >
                 <TokenReleased locked={locked} transferable={transferable} />
               </Grid>
@@ -165,50 +164,47 @@ export default class App extends React.Component {
               <TableVesting />
             </>
           ) : (
-              <>
-                <TokenVested withdrew={withdrewToken} available={availableToken} remain={remain} total={totalToken} />
-                <Button
-                  disabled={availableToken <= 0}
-                  variant="contained"
-                  fullWidth={true}
-                  onClick={this.buttonWithdrawMyToken.bind(this)}
-                  color={'primary'}
-                >
-                  Withdraw
-                </Button>
-                <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell sx={{ fontWeight: 'bold' }}>Term</TableCell>
-                        <TableCell sx={{ fontWeight: 'bold' }}>Data</TableCell>
+            <>
+              <TokenVested
+                clickWithdraw={this.buttonWithdrawMyToken.bind(this)}
+                withdrew={withdrewToken}
+                available={availableToken}
+                remain={remain}
+                total={totalToken}
+              />
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Term</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Data</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {displayData.map((row) => (
+                      <TableRow
+                        key={row.label.replace(/\s/g, '-')}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.label}
+                        </TableCell>
+                        <TableCell component="th" scope="row">
+                          {ethers.utils.isAddress(row.value) ? (
+                            <Link href={`https://bscscan.com/address/${row.value}`} target="_blank">
+                              {row.value}
+                            </Link>
+                          ) : (
+                            row.value
+                          )}
+                        </TableCell>
                       </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {displayData.map((row) => (
-                        <TableRow
-                          key={row.label.replace(/\s/g, '-')}
-                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                          <TableCell component="th" scope="row">
-                            {row.label}
-                          </TableCell>
-                          <TableCell component="th" scope="row">
-                            {ethers.utils.isAddress(row.value) ? (
-                              <Link href={`https://bscscan.com/address/${row.value}`} target="_blank">
-                                {row.value}
-                              </Link>
-                            ) : (
-                                row.value
-                              )}
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </>
-            )}
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
+          )}
         </Container>
       </>
     );
